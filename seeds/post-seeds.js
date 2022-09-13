7e22e6638810bf352e479d41e3c5c50299f80384
Verreauxx <tinon98713@gmail.com>
2022-09-12 23:06:49 -0700

    change

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,13 +2,13 @@ const { Post } = require('../models');
 
 const postdata = [
   {
-    title: 'Apple Releases New Iphones',
-    post_text: 'A magical new way to interact with iPhone. Groundbreaking safety features designed to save lives. An innovative 48MP camera for mind-blowing detail. All powered by the ultimate smartphone chip.',
+    title: 'Donec posuere metus vitae ipsum.',
+    post_text: 'Nam faucibus, magna vel elementum maximus, magna tellus tincidunt arcu, eu semper justo augue eget est.',
     user_id: 10
   },
   {
-    title: 'Google spins out secret hi-speed telecom project called Aalyria, and keeps stake in startup',
-    post_text: 'Inside Google, a team of techies has been working behind the scenes on software for high-speed communications networks that extend from land to space.',
+    title: 'Morbi non quam nec dui luctus rutrum.',
+    post_text: 'Donec purus arcu, ultricies dapibus ante et, ullamcorper porta eros.',
     user_id: 8
   },
   {
@@ -21,6 +21,87 @@ const postdata = [
     post_text: 'Nulla mollis placerat tellus.',
     user_id: 4
   },
+  {
+    title: 'Pellentesque eget nunc.',
+    post_text: 'Nulla mollis placerat tellus.',
+    user_id: 7
+  },
+  {
+    title: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    post_text: 'Nunc ut ornare metus.',
+    user_id: 4
+  },
+  {
+    title: 'In hac habitasse platea dictumst.',
+    post_text: ' Sed tempus massa quis augue bibendum, vitae consectetur tellus imperdiet.',
+    user_id: 1
+  },
+  {
+    title: 'Morbi non quam nec dui luctus rutrum.',
+    post_text: 'Praesent volutpat a neque et vestibulum.',
+    user_id: 1
+  },
+  {
+    title: 'Duis ac nibh.',
+    post_text: 'Cras tincidunt, mauris quis commodo lobortis, mauris augue tempor nisl, eget sagittis magna ex sed elit.',
+    user_id: 9
+  },
+  {
+    title: 'Curabitur at ipsum ac tellus semper interdum.',
+    post_text: 'Ut quam erat, auctor sit amet bibendum ut, posuere ut neque.',
+    user_id: 5
+  },
+  {
+    title: 'In hac habitasse platea dictumst.',
+    post_text: 'Vestibulum bibendum libero at mi euismod, et tincidunt arcu maximus.',
+    user_id: 3
+  },
+  {
+    title: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.',
+    post_text: 'Donec purus arcu, ultricies dapibus ante et, ullamcorper porta eros.',
+    user_id: 10
+  },
+  {
+    title: 'Donec dapibus.',
+    post_text: 'Aliquam nibh nibh, sagittis quis est sed, pellentesque finibus nisl.',
+    user_id: 8
+  },
+  {
+    title: 'Nulla tellus.',
+    post_text: 'Cras tincidunt, mauris quis commodo lobortis, mauris augue tempor nisl, eget sagittis magna ex sed elit.',
+    user_id: 3
+  },
+  {
+    title: 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo.',
+    post_text: 'Cras tincidunt, mauris quis commodo lobortis, mauris augue tempor nisl, eget sagittis magna ex sed elit.',
+    user_id: 3
+  },
+  {
+    title:
+      'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.',
+    post_text: 'Etiam accumsan tincidunt consectetur.',
+    user_id: 7
+  },
+  {
+    title: 'In hac habitasse platea dictumst.',
+    post_text: 'Donec purus arcu, ultricies dapibus ante et, ullamcorper porta eros.',
+    user_id: 6
+  },
+  {
+    title: 'Etiam justo.',
+    post_text: 'In ac suscipit turpis.',
+    user_id: 4
+  },
+  {
+    title: 'Nulla ut erat id mauris vulputate elementum.',
+    post_text: 'Nulla facilisi.',
+    user_id: 6
+  },
+  {
+    title: 'Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.',
+    post_text: 'Cras tincidunt, mauris quis commodo lobortis, mauris augue tempor nisl, eget sagittis magna ex sed elit.',
+    user_id: 7
+  }
 ];
 
 const seedPosts = () => Post.bulkCreate(postdata);
